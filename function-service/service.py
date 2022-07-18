9ea14580b5e6f4fdbaf8a15c1d1f73c7e5582d81
acse-dx121 <dx121@ic.ac.uk>
2022-07-18 15:29:14 +0800

    update

---
+++ function-service/service.py
@@ -0,0 +1,39 @@
+from concurrent import futures
+import imp
+import logging
+import math
+import time
+
+import grpc
+import impactEffect_pb2
+import impactEffect_pb2_grpc
+
+import impactEffects.instances.ImpactorClass
+from impactEffects.functions import *
+from impactEffects.functions.function import *
+from impactEffects.instances import ImpactorClass, TargetClass
+
+
+class ImpactEffectService(impactEffect_pb2_grpc.ImpactEffectServiceServicer):
+
+    def GetKineticEnergy(self, request, context):
+        print("-------------- GetKineticEnergy --------------")
+        # impactor = request.impactor
+        impactor = Impactor(diameter=request.diameter, density=request.density, velocity=request.velocity,
+                            theta=request.theta, depth=request.depth, ttype=request.ttype)
+        kinetic_energy = kinetic_energy(impactor)
+        return impactEffect_pb2.KineticEnergy(kinetic_energy=kinetic_energy)
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    impactEffect_pb2_grpc.add_ImpactEffectServiceServicer_to_server(
+        ImpactEffectService(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
