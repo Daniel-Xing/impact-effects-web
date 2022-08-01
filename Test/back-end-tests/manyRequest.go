c00b819483652d44721b55237a3298864f8ebcc8
acse-dx121 <dx121@ic.ac.uk>
2022-08-01 15:36:40 +0800

    update

--- Test/back-end-tests/manyRequest.go
+++ Test/back-end-tests/manyRequest.go
@@ -12,7 +12,7 @@ var tr *http.Transport
 
 var (
 	remoteURL = "https://impact.ese.ic.ac.uk/ImpactEarth/cgi-bin/crater.cgi?dist=12371&distanceUnits=1&diam=111&diameterUnits=1&pdens=111&pdens_select=0&vel=1111&velocityUnits=1&theta=45&tdens=1000&wdepth=1111&wdepthUnits=1"
-	localURL  = "http://localhost:50012/impact"
+	localURL  = "http://localhost:9998/api/admin/getCount/"
 )
 
 func init() {
@@ -40,7 +40,7 @@ func main() {
 	start := time.Now()
 	wg := sync.WaitGroup{}
 
-	t := 400
+	t := 100
 	fmt.Printf("Run request for %d times to with Redis URL: %s \n", t, localURL)
 	for i := 0; i < t; i++ {
 		wg.Add(1)
